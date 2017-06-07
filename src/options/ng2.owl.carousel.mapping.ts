83447069e42267c94a939320e31706bb47f3e740
sudip <sudipguti@gmail.com>
2017-06-08 03:22:02 +0530

    construct function added for mapping class

--- src/options/ng2.owl.carousel.mapping.ts
+++ src/options/ng2.owl.carousel.mapping.ts
@@ -3,9 +3,19 @@ export class ScreenResolutionMap {
     itemCount: number;
     nav: boolean;
     loop: boolean;
+    constructor(screenWidth: number, itemCount: number, nav: boolean, loop: boolean) {
+        this.screenWidth = screenWidth;
+        this.itemCount = itemCount;
+        this.nav = nav;
+        this.loop = loop;
+    }
 }
 
 export class MergefitMap {
     screenWidth: number;
     mergeFit: boolean;
+    constructor(screenWidth: number, mergeFit: boolean) {
+        this.screenWidth = screenWidth;
+        this.mergeFit = mergeFit;
+    }
 }
